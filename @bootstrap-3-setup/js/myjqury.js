82b1f795f3eacdb80447fd4b6cb6e2f40042bcd8
Arsalan Khalid <48867377+ArsalanKhalid07@users.noreply.github.com>
2019-06-16 09:55:44 +0500

    Add files via upload

---
+++ @bootstrap-3-setup/js/myjqury.js
@@ -0,0 +1,36 @@
+$('.first ').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.second ').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
